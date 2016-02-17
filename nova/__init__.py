b4d9153cd91b36e9a3187abc68b5562eeac1b486
Christer Edwards <christer.edwards@gmail.com>
2016-02-16 20:39:58 -0700

    simplifying and standardizing some core library functions

--- nova/__init__.py
+++ nova/__init__.py
@@ -11,13 +11,6 @@ LOG = logging.getLogger(__name__)
 
 GREP = salt.utils.which('egrep')
 STAT = salt.utils.which('stat')
-SYSCTL = salt.utils.which('sysctl')
-RPMQUERY = salt.utils.which('rpm')
-
-if salt.utils.which('chkconfig'):
-    CHKCONFIG = salt.utils.which('chkconfig')
-if salt.utils.which('systemctl'):
-    CHKCONFIG = salt.utils.which('systemctl')
 
 
 def _grep(pattern, filename, shell=False):
@@ -25,28 +18,18 @@ def _grep(pattern, filename, shell=False):
     return __salt__['cmd.run'](cmd, python_shell=shell)
 
 
-def _stat(filename):
-    '''
-    Standard function for all ``stat`` commands.
-    '''
+def _stat(filename, shell=False):
     cmd = '{0} {1} {2}'.format(STAT, '-L -c "%a %u %g"', filename)
-    return __salt__['cmd.run'](cmd, python_shell=False)
+    return __salt__['cmd.run'](cmd, python_shell=shell)
 
 
 def _sysctl(keyname):
-    cmd = '{0} {1}'.format(SYSCTL, keyname)
-    return __salt__['cmd.run'](cmd, python_shell=False)
+    return __salt__['sysctl.get'](keyname)
 
 
-def _rpmquery(package):
-    cmd = '{0} {1} {2}'.format(RPMQUERY, '-q', package)
-    return __salt__['cmd.run'](cmd, python_shell=False)
+def _package(package):
+    return __salt__['pkg.version'](package)
 
 
-def _chkconfig(service):
-    if 'systemctl' in CHKCONFIG:
-        cmd = '{0} {1} {2}'.format(CHKCONFIG, 'is-enabled', service)
-    elif 'chkconfig' in CHKCONFIG:
-        cmd = '{0} {1} {2}'.format(CHKCONFIG, '--list', service)
-    return __salt__['cmd.run'](cmd, python_shell=False)
-
+def _service(service):
+    return __salt__['service.status'](service)
