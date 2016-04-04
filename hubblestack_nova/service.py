5fd3099d681941d4fd930e1e14e6c2f59a6932bb
Colton Myers <colton.myers@gmail.com>
2016-04-04 12:23:10 -0600

    Add sample yaml for service.py, and the service.py audit module

---
+++ hubblestack_nova/service.py
@@ -0,0 +1,183 @@
+# -*- encoding: utf-8 -*-
+'''
+Hubble Nova plugin for auditing services
+
+Supports both blacklisting and "whitelisting" services. Blacklisted services
+must not be running. Whitelisted services must be running.
+
+:maintainer: HubbleStack
+:maturity: 20160404
+:platform: All
+:requires: SaltStack
+
+This audit module requires yaml data to execute. It will search the local
+directory for any .yaml files, and if it finds a top-level 'service' key, it will
+use that data.
+
+Sample YAML data, with inline comments:
+
+
+service:
+  # Must not be installed
+  blacklist:
+    # Unique ID for this set of audits
+    telnet:
+      data:
+        # 'osfinger' grain, for multiplatform support
+        CentOS Linux-6:
+          # service name : tag
+          - 'telnet': 'CIS-2.1.1'
+        # Catch-all, if no osfinger match was found
+        '*':
+          # service name : tag
+          - 'telnet': 'telnet-bad'
+      # description/alert/trigger are currently ignored, but may be used in the future
+      description: 'Telnet is evil'
+      alert: email
+      trigger: state
+  # Must be installed, no version checking (yet)
+  whitelist:
+    rsh:
+      data:
+        CentOS Linux-7:
+          - 'rsh': 'CIS-2.1.3'
+          - 'rsh-server': 'CIS-2.1.4'
+        '*':
+          - 'rsh-client': 'CIS-5.1.2'
+          - 'rsh-redone-client': 'CIS-5.1.2'
+          - 'rsh-server': 'CIS-5.1.3'
+          - 'rsh-redone-server': 'CIS-5.1.3'
+      description: 'RSH is awesome'
+      alert: email
+      trigger: state
+
+'''
+from __future__ import absolute_import
+import logging
+
+import fnmatch
+import yaml
+import os
+import copy
+import salt.utils
+
+from distutils.version import LooseVersion
+
+log = logging.getLogger(__name__)
+
+__tags__ = None
+__data__ = None
+
+
+def __virtual__():
+    if salt.utils.is_windows():
+        return False, 'This audit module only runs on linux'
+    global __tags__
+    global __data__
+    yamldir = os.path.dirname(__file__)
+    __data__ = _get_yaml(yamldir)
+    __tags__ = _get_tags(__data__)
+    return True
+
+
+def audit(tags, verbose=False):
+    '''
+    Run the service audits contained in the YAML files processed by __virtual__
+    '''
+    ret = {'Success': [], 'Failure': []}
+    for tag in __tags__:
+        if fnmatch.fnmatch(tag, tags):
+            for tag_data in __tags__[tag]:
+                name = tag_data['name']
+                audittype = tag_data['type']
+
+                # Blacklisted packages (must not be installed)
+                if audittype == 'blacklist':
+                    if __salt__['service.status'](name):
+                        ret['Failure'].append(tag_data)
+                    else:
+                        ret['Success'].append(tag_data)
+
+                # Whitelisted packages (must be installed)
+                elif audittype == 'whitelist':
+                    if __salt__['service.status'](name):
+                        ret['Success'].append(tag_data)
+                    else:
+                        ret['Failure'].append(tag_data)
+
+    if not verbose:
+        failure = set()
+        success = set()
+
+        for tag_data in ret['Failure']:
+            tag = tag_data['tag']
+            failure.add(tag)
+
+        for tag_data in ret['Success']:
+            tag = tag_data['tag']
+            if tag not in failure:
+                success.add(tag)
+
+        ret['Success'] = list(success)
+        ret['Failure'] = list(failure)
+
+    return ret
+
+
+def _get_yaml(dirname):
+    '''
+    Iterate over the current directory for all yaml files, read them in,
+    merge them, and return the __data__
+    '''
+    ret = {}
+    try:
+        for yamlpath in os.listdir(dirname):
+            if yamlpath.endswith('.yaml'):
+                with open(os.path.join(dirname, yamlpath)) as fh_:
+                    data = yaml.safe_load(fh_)
+                _merge_yaml(ret, data)
+    except:
+        return {}
+    return ret
+
+
+def _merge_yaml(ret, data):
+    '''
+    Merge two yaml dicts together at the service:blacklist and service:whitelist level
+    '''
+    if 'service' not in ret:
+        ret['service'] = {}
+    for topkey in ('blacklist', 'whitelist'):
+        if topkey in data.get('service', {}):
+            if topkey not in ret['service']:
+                ret['service'][topkey] = {}
+            ret['service'][topkey].update(data['service'][topkey])
+    return ret
+
+
+def _get_tags(data):
+    '''
+    Retrieve all the tags for this distro from the yaml
+    '''
+    ret = {}
+    distro = __grains__.get('osfinger')
+    for toplist, toplevel in data.get('service', {}).iteritems():
+        # service:blacklist
+        for audit_id, audit_data in toplevel.iteritems():
+            # service:blacklist:telnet
+            tags_dict = audit_data.get('data', {})
+            # service:blacklist:telnet:data
+            tags = tags_dict.get(distro, tags_dict.get('*', []))
+            # service:blacklist:telnet:data:Debian-8
+            for item in tags:
+                for name, tag in item.iteritems():
+                    if tag not in ret:
+                        ret[tag] = []
+                    formatted_data = {'name': name,
+                                      'tag': tag,
+                                      'module': 'service',
+                                      'type': toplist}
+                    formatted_data.update(audit_data)
+                    formatted_data.pop('data')
+                    ret[tag].append(formatted_data)
+    return ret
