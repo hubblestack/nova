2cf17b1baad613f62c372ee39ebc6fc95f48e54d
Gabriel Miu <miu@adobe.com>
2016-04-17 05:55:18 +0300

    Added docstring

--- hubblestack_nova/sysctl.py
+++ hubblestack_nova/sysctl.py
@@ -1,3 +1,34 @@
+# -*- encoding: utf-8 -*-
+'''
+Hubble Nova plugin for using sysctl to verify sysctl parameter
+
+:maintainer: HubbleStack
+:maturity: 20160417
+:platform: CentOS-6 and CentOS-7
+:requires: SaltStack
+
+This audit module requires yaml data to execute. It will search the local
+directory for any .yaml files, and if it finds a top-level 'sysctl' key, it will
+use that data.
+
+Sample YAML data, with inline comments:
+
+sysctl:
+  randomize_va_space:  # unique ID
+    data:
+      'CentOS-6':  #osfinger grain
+        - 'kernel.randomize_va_space':  #sysctl param to check
+            tag: 'CIS-1.6.3'  #audit tag
+            match_output: '2'   #expected value of the checked parameter
+      'CentOS-7':
+        - 'kernel.randomize_va_space':
+            tag: 'CIS-1.6.2'
+            match_output: '2'
+    description: 'Enable Randomized Virtual Memory Region Placement (Scored)'
+    alert: email
+    trigger: state
+'''
+
 from __future__ import absolute_import
 import logging
 
