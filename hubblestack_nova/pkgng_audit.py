55f3cd6d261ae899594f232e4912b1e32e7b0c4f
Christer Edwards <christer.edwards@gmail.com>
2016-04-22 13:01:12 -0600

    initial support for pkg.audit (FreeBSD)

---
+++ hubblestack_nova/pkgng_audit.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+'''
+Hubble Nova plugin for FreeBSD pkgng audit
+
+:maintainer: HubbleStack
+:maturity: 20160421
+:platform: FreeBSD
+:requires: SaltStack
+'''
+from __future__ import absolute_import
+import logging
+
+log = logging.getLogger(__name__)
+
+__tags__ = None
+
+
+def __virtual__():
+    if 'FreeBSD' not in __grains__['os']:
+        return False, 'This audit module only runs on FreeBSD'
+    global __tags__
+    return True
+
+
+def audit(tags, verbose=False):
+    '''
+    Run the pkg.audit command
+    '''
+    ret = {'Success': [], 'Failure': []}
+
+    salt_ret = __salt__['pkg.audit']()
+    if '0 problem(s)' not in salt_ret:
+        ret['Failure'].append(salt_ret)
+    else:
+        ret['Success'].append(salt_ret)
+
+    return ret
