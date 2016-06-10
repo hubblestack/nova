7ceeb8081908229ffa3a951db7d0855533349840
Chad Mills <chadlmills@gmail.com>
2016-06-10 16:52:02 -0600

    added win_auditpol and win_pkg.  Also made changes to yaml file

--- hubblestack_nova/modules/win_auditpol.py
+++ hubblestack_nova/modules/win_auditpol.py
@@ -1,47 +1,41 @@
-# auditpol.py
-# -*- encoding: utf-8 -*-
+# win_auditpol.py
 '''
 Loader and primary interface for nova modules
 
 :maintainer: HubbleStack
-:maturity: 20160426
+:maturity: 20160526
 :platform: Windows
 :requires: SaltStack
 :TODO:
 '''
-from __future__ import absolute_import
-import logging
 
-import fnmatch
+from __future__ import absolute_import
 import copy
+import csv
+import fnmatch
+import logging
 import salt.utils
 
-try:
-    import uuid
-    import csv
-    HAS_WINDOWS_MODULES = True
-except ImportError:
-    HAS_WINDOWS_MODULES = False
 
 log = logging.getLogger(__name__)
-__virtualname__ = 'auditpol'
+__virtualname__ = 'win_auditpol'
 
 def __virtual__():
-    if not salt.utils.is_windows() or not HAS_WINDOWS_MODULES:
-        return False, 'This audit module only runs on Windows'
+    if not salt.utils.is_windows():
+        return False, 'This audit module only runs on windows'
     return True
 
 
 def audit(data_list, tags, verbose=False):
-    '''
-    Run the auditpol audits contained in the YAML files processed by __virtual__
-    '''
+    '''Runs auditpol on the local machine and audits the return data
+    with the CIS yaml processed by __virtual__'''
     __data__ = {}
+    __auditdata__ = _auditpol_import()
     for data in data_list:
         _merge_yaml(__data__, data)
     __tags__ = _get_tags(__data__)
     log.trace('auditpol audit __data__:')
-    log.trace('__data__')
+    log.trace(__data__)
     log.trace('auditpol audit __tags__:')
     log.trace(__tags__)
 
@@ -53,47 +47,28 @@ def audit(data_list, tags, verbose=False):
                     ret['Controlled'].append(tag_data)
                     continue
                 name = tag_data['name']
-                audittype = tag_data['type']
+                audit_type = tag_data['type']
+                match_output = tag_data['match_output'].lower()
 
                 # Blacklisted audit (do not include)
-                if audittype == 'blacklist':
-                    auditpol_ret = _find_option_value_in_auditpolfile(tag_data['name'])
-                    if 'binary' in tag_data['value_type']:
-                        auditpol_ret = _binary_convert(auditpol_ret)
-                    elif 'multi' in tag_data['value_type']:
-                        auditpol_ret = _multi_convert(auditpol_ret)
-                    elif 'less' or 'more' or 'equal' in tag_data['value_type']:
-                        auditpol_ret = _get_operation(auditpol_ret,
-                                                     tag_data['value_type'],
-                                                     tag_data['match_output'])
-
-                    found = False
-                    if auditpol_ret:
-                        found = True
-                    if 'match_output' in tag_data and tag_data['match_output'] not in auditpol_ret:
-                        found = False
-
-                    if found:
-                        ret['Failure'].append(tag_data)
-                    else:
-                        ret['Success'].append(tag_data)
-
-                # Whitelisted packages (must be installed)
-                elif audittype == 'whitelist':
-                    auditpol_ret = _find_option_value_in_auditpolfile(tag_data['name'])
-                    if 'multi' in tag_data['value_type']:
-                        auditpol_ret = _multi_convert(auditpol_ret)
-
-                    found = False
-                    if auditpol_ret:
-                        found = True
-                    if 'match_output' in tag_data and tag_data['match_output'] not in auditpol_ret:
-                        found = False
-
-                    if found:
+                if 'blacklist' in audit_type:
+                    if name not in __auditdata__:
                         ret['Success'].append(tag_data)
                     else:
                         ret['Failure'].append(tag_data)
+
+                # Whitelisted audit (must include)
+                if 'whitelist' in audit_type:
+                    if name in __auditdata__:
+                        audit_value = __auditdata__[name].lower()
+                        secret = _translate_value_type(audit_value, tag_data['value_type'], match_output)
+                        if secret:
+                            ret['Success'].append(tag_data)
+                        else:
+                            ret['Failure'].append(tag_data)
+                    else:
+                        log.debug('When trying to audit the advanced auditpol section,'
+                                  ' the yaml contained incorrect data for the key')
 
     if not verbose:
         failure = []
@@ -124,10 +99,10 @@ def audit(data_list, tags, verbose=False):
             tag = tag_data['tag']
             control_reason = tag_data.get('control', '')
             description = tag_data.get('description')
-            if (tag, description, control_reason) not in control_reasons:
+            if (tag, description, control_reason) not in tags_descriptions:
                 tag_dict = {'description': description,
                             'control': control_reason}
-                controlled.append({tag: tag_dict})
+                controlled.append({tag: description})
                 control_reasons.add((tag, description, control_reason))
 
         ret['Controlled'] = controlled
@@ -140,45 +115,19 @@ def audit(data_list, tags, verbose=False):
     return ret
 
 
-def _find_option_value_in_auditpolfile(option):
-    '''
-    helper function to dump/parse an `auditpol /backup` file for a particular option
-    '''
-    try:
-        _d = uuid.uuid4().hex
-        _tfile = '{0}\\{1}'.format(__salt__['config.get']('cachedir'), 'salt-auditpol-dump-{0}.txt'.format(_d))
-        _ret = __salt__['cmd.run']('auditpol /backup /file:{0}'.format(_tfile))
-        if _ret:
-            csvfile = open(_tfile, 'rb')
-            try:
-                _reader = csv.reader(csvfile)
-                for _line in _reader:
-                    if option in _line[2] and 'RegistryGlobalSacl' not in _line[2]:
-                        result = _line[6]
-            finally:
-                csvfile.close()
-                _ret = __salt__['file.remove'](_tfile)
-                return result
-        return True, 'Not Defined'
-    except:
-        log.debug('error occurred while trying to get auditpol data')
-        return False, None
-
-
-
 def _merge_yaml(ret, data):
     '''
-    Merge two yaml dicts together at the auditpol:blacklist and
-    auditpol:whitelist level
+    Merge two yaml dicts together at the secedit:blacklist and
+    secedit:whitelist level
     '''
-    if 'auditpol' not in ret:
-        ret['auditpol'] = {}
+    if __virtualname__ not in ret:
+        ret[__virtualname__] = {}
     for topkey in ('blacklist', 'whitelist'):
-        if topkey in data.get('auditpol', {}):
-            if topkey not in ret['auditpol']:
-                ret['auditpol'][topkey] = []
-            for key, val in data['auditpol'][topkey].iteritems():
-                ret['auditpol'][topkey].append({key: val})
+        if topkey in data.get(__virtualname__, {}):
+            if topkey not in ret[__virtualname__]:
+                ret[__virtualname__][topkey] = []
+            for key, val in data[__virtualname__][topkey].iteritems():
+                ret[__virtualname__][topkey].append({key: val})
     return ret
 
 
@@ -188,13 +137,13 @@ def _get_tags(data):
     '''
     ret = {}
     distro = __grains__.get('osfullname')
-    for toplist, toplevel in data.get('auditpol', {}).iteritems():
-        # auditpol:whitelist
+    for toplist, toplevel in data.get(__virtualname__, {}).iteritems():
+        # secedit:whitelist
         for audit_dict in toplevel:
             for audit_id, audit_data in audit_dict.iteritems():
-                # auditpol:whitelist:PasswordComplexity
+                # secedit:whitelist:PasswordComplexity
                 tags_dict = audit_data.get('data', {})
-                # auditpol:whitelist:PasswordComplexity:data
+                # secedit:whitelist:PasswordComplexity:data
                 tags = None
                 for osfinger in tags_dict:
                     if osfinger == '*':
@@ -209,7 +158,7 @@ def _get_tags(data):
                 # If we didn't find a match, check for a '*'
                 if tags is None:
                     tags = tags_dict.get('*', [])
-                # auditpol:whitelist:PasswordComplexity:data:Debian-8
+                # secedit:whitelist:PasswordComplexity:data:Windows 2012
                 if isinstance(tags, dict):
                     # malformed yaml, convert to list of dicts
                     tmp = []
@@ -227,7 +176,7 @@ def _get_tags(data):
                             ret[tag] = []
                         formatted_data = {'name': name,
                                           'tag': tag,
-                                          'module': 'auditpol',
+                                          'module': 'win_auditpol',
                                           'type': toplist}
                         formatted_data.update(tag_data)
                         formatted_data.update(audit_data)
@@ -236,21 +185,31 @@ def _get_tags(data):
     return ret
 
 
-def _multi_convert(val, **kwargs):
-    '''
-    converts an audit setting # (0, 1, 2, 3) to the string text
-    '''
-
-    if val is not None:
-        if val == 0 or val == "0":
-            return 'No auditing'
-        elif val == 1 or val == "1":
-            return 'Success'
-        elif val == 2 or val == "2":
-            return 'Failure'
-        elif val == 3 or val == "3":
-            return 'Success, Failure'
+def _auditpol_export():
+    try:
+        dump = __salt__['cmd.run']('auditpol /get /category:* /r')
+        if dump:
+            dump = dump.split('\n')
+            return dump
         else:
-            return 'Invalid Auditing Value'
-    else:
-        return 'Not Defined'
+            log.error('Nothing was returned from the auditpol command.')
+    except StandardError:
+        log.error('An error occurred running the auditpol command.')
+
+
+def _auditpol_import():
+    dict_return = {}
+    export = _auditpol_export()
+    auditpol_csv = csv.DictReader(export)
+    for row in auditpol_csv:
+        if row:
+            dict_return[row['Subcategory']] = row['Inclusion Setting']
+    return dict_return
+
+
+def _translate_value_type(current, value, evaluator):
+    if 'equal' in value:
+        if current == evaluator:
+            return True
+        else:
+            return False
