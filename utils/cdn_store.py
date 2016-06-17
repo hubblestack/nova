2bd10da3df1a30657f2e99d3e796d4375e0df041
jaredhanson11 <jred0011@gmail.com>
2016-06-17 14:42:27 -0600

    Minor fixes in cdn_store.py

--- utils/cdn_store.py
+++ utils/cdn_store.py
@@ -1,14 +1,23 @@
+'''
+This module saves all cve scans for each distribution in a json file.
+    Pulls cve scans from www.vulners.com/api/v3
+'''
+
 import urllib2
 import json
 
 def main():
+    '''
+    Calls helpers and saves results.
+    '''
     distro_list = ['centos', 'ubuntu', 'debian']
     for distro in distro_list:
-        unencoded_json = get_json(distro)
-        json.dump(unencoded_json, open('distro_json/%s.json' % distro, 'w')) ## Path to directory here
-        print 'Saved: %s.json' % distro
+        unencoded_json = _get_json(distro)
+        json.dump(unencoded_json, open('distro_json/%s.json' % distro, 'w')) #Change path
+        print 'Saved: %s.json\n' % distro
 
-def get_json(os_name):
+
+def _get_json(os_name):
     '''
     Returns json from vulner.com api for specified os_name
     '''
@@ -18,14 +27,15 @@ def get_json(os_name):
 
     while is_next_page:
         offset = page_num * query_size
-        page_num += 1 
+        page_num += 1
         print "Grabbing page: %s of %s" % (page_num, os_name)
-        url_final = 'http://vulners.com/api/v3/search/lucene/?query=type:%s&skip=%s&size=%s' % (os_name, offset, query_size)
+        url_final = 'http://vulners.com/api/v3/search/lucene/' \
+                    '?query=type:%s&skip=%s&size=%s' % (os_name, offset, query_size)
         cve_query = urllib2.urlopen(url_final)
-        cve_json = json.load(cve_query)
-        
-        # Default number of searches per page is 20 so 
-        #    if we have less than that we know this is 
+        cve_json = json.loads(cve_query.read())
+
+        # Default number of searches per page is 'size:' so
+        #    if we have less than that we know this is
         #    our last page.
         if len(cve_json['data']['search']) < query_size:
             is_next_page = False
@@ -34,8 +44,9 @@ def get_json(os_name):
         if page_num == 1:
             master_json = cve_json
             continue
-        
+
         master_json = _build_json(master_json, cve_json)
+    return master_json
 
 
 def _build_json(master_json, current_page):
