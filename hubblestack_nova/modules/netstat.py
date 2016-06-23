6a8f7b3ddf6c43565efeda5d21de714808e98785
Colton Myers <colton.myers@gmail.com>
2016-06-23 13:18:57 -0600

    Add sample yaml data

--- hubblestack_nova/modules/netstat.py
+++ hubblestack_nova/modules/netstat.py
@@ -6,6 +6,16 @@ Hubble Nova plugin for FreeBSD pkgng audit
 :maturity: 20160623
 :platform: Unix
 :requires: SaltStack
+
+Sample data for the netstat whitelist:
+
+.. code-block:: yaml
+
+    netstat:
+        ssh:
+            address: 0.0.0.0:22
+        another_identifier:
+            address: 127.0.0.1:80
 '''
 from __future__ import absolute_import
 
