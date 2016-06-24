d8dd80d44f8974bba1cf63b1492638af5f9d376b
Jared Hanson <jred0011@gmail.com>
2016-06-24 11:17:22 -0600

    renamed script, edited documentation

--- utils/cve_store.py
+++ utils/cve_store.py
@@ -1,9 +1,10 @@
 '''
 A script that will query vulners.com/api for cve data related to given operating systems.
 The data is returned in a valid json format for use in the cve_scan_v2 module. The json file
-is stored at the local directory under <os_name>_<version>.json.
+is stored at the local directory under <os_name>_<version>.json. Inputs must be in the form
+os-version, like 'centos-7' or 'ubuntu-16.04' etc.
 
-usage:
+usage: # python cve_store.py (<os-version>) [<os-version> ...]
 '''
 
 import requests
