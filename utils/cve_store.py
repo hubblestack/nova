69185c688f0b8051fe4076720b428853f1e1db42
Jared Hanson <jred0011@gmail.com>
2016-06-27 14:00:23 -0600

    Linted cve_store

--- utils/cve_store.py
+++ utils/cve_store.py
@@ -6,17 +6,15 @@ os-version, like 'centos-7' or 'ubuntu-16.04' etc.
 
 usage: # python cve_store.py (<os-version>) [<os-version> ...]
 '''
-
-import requests
-import json
 from zipfile import ZipFile
 import os
 import sys
+import requests
 
 def main():
     '''
     Tries to save cve scans for inputs. Specify type and version.
-        Ex: # python cdn_store.py centos-7 ubuntu-16.04
+        Ex: # python cve_store.py centos-7 ubuntu-16.04
     '''
     if len(sys.argv) == 1:
         print "No inputs were given."
@@ -24,15 +22,15 @@ def main():
         print
         try:
             _save_json(distro)
-        except Exception, e:
+        except Exception, exc:
             print 'Error saving: %s' % distro
-            print e
+            print exc
 
 
 def _save_json(distro):
     '''
     Returns json from vulner.com api for specified distro.
-    Throws exceptions when erros occured to be caught by main.
+    Exceptions thrown are caught by main()
     '''
     split = distro.split('-')
     if len(split) != 2:
@@ -40,7 +38,8 @@ def _save_json(distro):
     version = split[1]
     distro_name = split[0]
     print 'Getting cve\'s for %s version %s' % (distro_name, version)
-    url_final = 'http://www.vulners.com/api/v3/archive/distributive/?os=%s&version=%s' % (distro_name, version)
+    url_final = 'http://www.vulners.com/api/v3/archive/distributive/?os=%s&version=%s' \
+                                                                % (distro_name, version)
     cve_query = requests.get(url_final)
     # Filenames returned don't contain periods.
     version = version.replace('.', '')
@@ -55,8 +54,6 @@ def _save_json(distro):
     zip_file = ZipFile(_zip)
     zip_file.extractall(os.path.dirname(_zip))
     os.remove(_zip)
-    with open(_json, 'r') as json_file:
-        master_json = json.load(json_file)
     print 'Saved: %s' % _json
 
 
